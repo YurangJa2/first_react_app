e0287461a4ef61ef5d14aba3a6cc76faa76733ca
YurangJa2 <sang990701@gmail.com>
2020-07-05 22:36:23 +0900

    using proxy to connect with backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [message, setMessage] = useState("");
+  // useEffect: 기존의 componentDidMount, componentDidUpdate, componentWillUnmount가 합쳐진 것으로 생각해도 된다.
+  useEffect(() => {
+    fetch('/api/hello').then(response => response.text())
+                       .then(message => {
+                         setMessage(message);
+                       });
+  }, [])
+
   return (
-    <div><h1>Hello React!</h1></div>
+    <div>
+      <h1>Hello React!</h1>
+      <h2>{message}</h2>
+    </div>  
   );
 }
 
