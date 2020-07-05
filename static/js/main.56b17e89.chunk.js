febbb88cd5b58eaaf16b7b72bf06bbecc3837d8d
YurangJa2 <sang990701@gmail.com>
2020-07-05 15:04:17 +0900

    Updates

---
+++ static/js/main.56b17e89.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfirst_react_app=this.webpackJsonpfirst_react_app||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2);var l=function(){return r.a.createElement("div",null,r.a.createElement("h1",null,"Hello React!"))};n.n(c).a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(l,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.56b17e89.chunk.js.map
