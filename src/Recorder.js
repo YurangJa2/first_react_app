ffc67114102d535e88f7db3a6c1ca43758190c14
YurangJa2 <sang990701@gmail.com>
2020-08-03 22:29:04 +0900

    callback function for mobile environment done

--- src/Recorder.js
+++ src/Recorder.js
@@ -10,12 +10,20 @@ class Recorder extends React.Component {
     PipeSDK.insert("custom-id",pipeParams,function(recorderObject){
       recorderObject.onUploadDone = function(recorderId, streamName, streamDuration, audioCodec, videoCodec, fileType, audioOnly, location){
         var args = Array.prototype.slice.call(arguments);
-        console.log(arguments);
+        console.log("uploading done " + arguments);
+        alert("https://eu1-addpipe.s3.eu-central-1.amazonaws.com/e8a9098088c1db348ae13b82a06941cc/" + streamName + ".mp4");
         localStorage.setItem("video", "https://eu1-addpipe.s3.eu-central-1.amazonaws.com/e8a9098088c1db348ae13b82a06941cc/" + streamName + ".mp4");
       },
       recorderObject.onDesktopVideoUploadSuccess = function(recorderId, filename,filetype,videoId,audioOnly,location){
         var args = Array.prototype.slice.call(arguments);
-        console.log(arguments);
+        console.log("uploading done " + arguments);
+        alert("https://eu1-addpipe.s3.eu-central-1.amazonaws.com/e8a9098088c1db348ae13b82a06941cc/" + filename + "." + filetype);
+        localStorage.setItem("video", "https://eu1-addpipe.s3.eu-central-1.amazonaws.com/e8a9098088c1db348ae13b82a06941cc/" + filename + "." + filetype);
+      },
+      recorderObject.onVideoUploadSuccess = function(recorderId, filename,filetype,videoId,audioOnly,location){
+        var args = Array.prototype.slice.call(arguments);
+        console.log("uploading mobile done " + arguments);
+        alert("https://eu1-addpipe.s3.eu-central-1.amazonaws.com/e8a9098088c1db348ae13b82a06941cc/" + filename + "." + filetype);
         localStorage.setItem("video", "https://eu1-addpipe.s3.eu-central-1.amazonaws.com/e8a9098088c1db348ae13b82a06941cc/" + filename + "." + filetype);
       }
     });
